66c0b4288e2e58c3a71263f544318971c8ad1c67
PhiseySt <phstas2016@rambler.ru>
2022-10-19 15:20:17 +0300

    Add ArrayStack

---
+++ ArrayStack/ArrayStack/ArrayStack.cs
@@ -0,0 +1,57 @@
+﻿namespace ArrayStack;
+
+public class ArrayStack<T>
+{
+    private T[] _items;
+    public int Count { get; set; }
+    private const int ItemsDefaultLength = 10;
+
+    public ArrayStack()
+    {
+        _items = new T[ItemsDefaultLength];
+    }
+
+    public ArrayStack(int userLength)
+    {
+        _items = new T[userLength];
+    }
+
+    public bool IsEmpty() => Count == 0;
+
+    public void Push(T item)
+    {
+        if (Count == _items.Length)
+        {
+            Resize(_items.Length + 10);
+        }
+
+        _items[Count++] = item;
+    }
+
+    private void Resize(int newLength)
+    {
+        var newItems = new T[newLength];
+        for (var i = 0; i < Count; i++)
+        {
+            newItems[i]= _items[i];
+        }
+        _items = newItems;
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty()) throw new InvalidOperationException("Стек пуст");
+        T item = _items[--Count];
+        _items[Count] = default;
+        if (Count > 0 && Count < _items.Length - 10)
+        {
+            Resize(_items.Length - 10);
+        }
+        return item;
+    }
+
+    public T Peek()
+    {
+        return _items[Count - 1];
+    }
+}
---
+++ ArrayStack/ArrayStack/Program.cs
@@ -0,0 +1,34 @@
+﻿using ArrayStack;
+
+try
+{
+    var arrayStack = new ArrayStack<string>(8);
+
+    arrayStack.Push("Player1");
+    arrayStack.Push("Player2");
+    arrayStack.Push("Player3");
+    arrayStack.Push("Player4");
+    arrayStack.Push("Player5");
+    arrayStack.Push("Player6");
+    arrayStack.Push("Player7");
+    arrayStack.Push("Player8");
+    arrayStack.Push("Player9");
+    arrayStack.Push("Player10");
+    arrayStack.Push("Player11");
+    arrayStack.Push("Player12");
+
+
+    var head = arrayStack.Pop();
+    Console.WriteLine(head);   
+
+
+    var head2 = arrayStack.Peek();
+    Console.WriteLine(head2);   
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+Console.Read();
